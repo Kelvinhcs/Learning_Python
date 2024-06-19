73f19f30078758d6cba8846897b4f134bd1b153b
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-06-19 20:51:32 -0300

    19/06/2024

---
+++ ArquivosPy/Exercicio75-Comentarios.py
@@ -0,0 +1,1 @@
+tupla = tuple(int(input(f'Digite o {c}° valor: ')) for c in range(1, 5))
--- ArquivosPy/Exercicio75.py
+++ ArquivosPy/Exercicio75.py
@@ -2,8 +2,15 @@ n1 = int(input('Digite o primeiro valor: '))
 n2 = int(input('Digite o segundo valor: '))
 n3 = int(input('Digite o terceiro valor: '))
 n4 = int(input('Digite o quarto valor: '))
-lista = (n1,n2,n3,n4)
-print(f'O número 9 apareceu {lista.count(9)} vez(es).' if lista.count(9) >= 1 else f'O número 9 Não apareceu nenhuma vez.')
-print(f'O número 3 foi digitado na posição {lista.index(3)+1}.' if lista.index(3) >= 0 else f'O número 3 não foi digitado em nenhuma posição.')
+tupla = (n1,n2,n3,n4)
+pares = 0
+print(f'O número 9 apareceu {tupla.count(9)} vez(es).' if tupla.count(9) >= 1 else 'O número 9 Não apareceu nenhuma vez.')
+print(f'O número 3 apareceu na posição {tupla.index(3)+1}.' if 3 in tupla else 'O número 3 Não apareceu nenhuma vez.')
+print(f'Os números pares digitados foram: ', end='')
+for c in tupla:
+    if c % 2 == 0:
+        pares += 1
+        print(f'{c} ', end='')
+if pares == 0:
+    print('NENHUM número par encontrado.')
 
-#UNSOLVED
