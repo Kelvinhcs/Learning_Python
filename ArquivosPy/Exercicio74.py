4d5a54760bb76862d1aedf644a23c31513f0e42e
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-06-19 19:54:01 -0300

    complementing

--- ArquivosPy/Exercicio74.py
+++ ArquivosPy/Exercicio74.py
@@ -1,5 +1,9 @@
 from random import randint
 lista = (randint(0,9), randint(0,9), randint(0,9), randint(0,9), randint(0,9))
-print(f'Os números gerados foram {lista}')
-print(f'O menor número é {sorted(lista)[0]}')
-print(f'O maior número é {sorted(lista, reverse=True)[0]}')
+print(f'Os números gerados foram {lista}.')
+print(f'O menor número (sorted) é {sorted(lista)[0]}.')
+print(f'O menor valor (min() ) é {min(lista)}.')
+print('-=-'*20)
+print(f'O maior número (sorted com reverse=True) é {sorted(lista, reverse=True)[0]}.')
+print(f'O maior número (sorted apresentando o último valor) é {sorted(lista)[-1]}.')
+print(f'O maior valor (max() ) é {max(lista)}.')
