fb910579a268b79aba94a7f03c4a926e993b3e93
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-06-24 03:01:46 -0300

    Update Teste.py

--- ArquivosPy/Teste.py
+++ ArquivosPy/Teste.py
@@ -1,13 +1,33 @@
-while True:
-    resposta = ''
-    while resposta not in ('s', 'n'):
-        resposta = str(input('Quer continuar [s/n] ? ')).strip().lower()[0]
-        if resposta not in ('s', 'n'):
-            print(f'Resposta Inválida, tente novamente...')
-    if resposta == 'n':
-        break
-    if resposta == 's':
-        print('Digitei certo, fechando...')
-        break
-print('Fim')
-        
+#ListaGeral = [['Pedro',[9.5, 5.2]], ['Kelvin', [6.2, 5.0]], ['MatGx'[10.0, 5.0]]]
+#ListaGeral = [[9.5, 5.2]]
+ListaGeral = []
+placeholder = []
+for c in range(0, 2):
+    placeholder.clear()
+    NomeDigitado = [str(input('Nome do aluno: ')).strip().capitalize()]
+    print(NomeDigitado)
+    Notas = [float(input(f'Digite a 1ª nota: ')), float(input(f'Digite a 1ª nota: '))]
+    ListaGeral.append(NomeDigitado[:])
+    print(Notas)
+    ListaGeral.append(Notas[:])
+    print(ListaGeral)
+    ListaGeral
+
+[[9.5, 5.2, ['Pedro']]]
+###encontrei a opção de gerar uma lista vazia antes e apagala para assim poder utilizar o indice
+"""
+ListaGeral = []
+placeholder = []
+for c in range(0, 2):
+    placeholder.clear()
+    NomeDigitado = str(input('Nome do aluno: ')).strip().capitalize()
+    Nota1 = float(input(f'Digite a 1ª nota: '))
+    Nota2 = float(input(f'Digite a 2ª nota: '))
+    placeholder.append(Nota1)
+    placeholder.append(Nota2)
+    ListaGeral.append(list('nada'))
+    ListaGeral[c].clear()
+    ListaGeral[c].append(placeholder[:])
+    ListaGeral[c].append(NomeDigitado)
+print(ListaGeral)
+"""
