205e251cd20c309b9e0709428a71661e32a9c9ff
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-06-24 22:46:57 -0300

    Update Exercicio86-Simplificado.py

--- ArquivosPy/Exercicio86-Simplificado.py
+++ ArquivosPy/Exercicio86-Simplificado.py
@@ -1,10 +1,10 @@
-listagem = [[[],[],[]], [[],[],[]], [[], [], []]]
-for principal in range(0, 3):
-    for contador in range(0,3):
-        digitado = int(input(f'Digite um valor para posição [{principal}, {contador}]: '))
-        listagem[principal][contador].append(digitado)
-for item in listagem:
-    for vezes, coisa in enumerate(item):
-        print(coisa, end='')
+Matriz = [[[],[],[]], [[],[],[]], [[], [], []]]
+for Linha in range(0, 3):
+    for Coluna in range(0,3):
+        Digitado = int(input(f'Digite um valor para posição [{Linha}, {Coluna}]: '))
+        Matriz[Linha][Coluna].append(Digitado)
+for Linha in Matriz:
+    for Coluna in Linha:
+        print(Coluna, end='')
     print()
             
