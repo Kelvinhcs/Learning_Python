563942b8ca7a501884c2c9b8d6e473653f61e9bd
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-06-24 22:34:31 -0300

    Update Exercicio85-1Lista.py

--- ArquivosPy/Exercicio85-1Lista.py
+++ ArquivosPy/Exercicio85-1Lista.py
@@ -1,13 +1,13 @@
-listagem = [[], []]
+Listagem = [[], []]
 print('-=-'*22)
-for contador in range(0, 7):
-    numero = int(input(f'Digite o {contador+1}º número: '))
-    if numero % 2 == 0:
-        listagem[0].append(numero)
+for Contador in range(0, 7):
+    NumeroDigitado = int(input(f'Digite o {Contador+1}º número: '))
+    if NumeroDigitado % 2 == 0:
+        Listagem[0].append(NumeroDigitado)
     else:
-        listagem[1].append(numero)
+        Listagem[1].append(NumeroDigitado)
 print('-=-'*22)
-listagem[0].sort() #Existe a possibilidade de utilizar o metodo "sorted()" ao inves de sortear a lista,
-listagem[1].sort() #Assim mantendo a lista original utilizando o sorted() no momento do print.
-print(f'Os números pares são {listagem[0]}.')
-print(f'Os números ímpares são {listagem[1]}.')
+print(f'Os números pares são {sorted(Listagem[0])}.')
+print(f'Os números ímpares são {sorted(Listagem[1])}.')
+
+#Existe a possibilidade de utilizar o metodo "sorted()" ao inves de sortear a lista, Assim mantendo a lista original utilizando o sorted() no momento do print. Originalmente Utilizei listagem[0].sort() e listagem[1].sort()
