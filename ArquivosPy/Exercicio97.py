bdd94b4e3ba92bf9296b6b39beb9c736169fcf96
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-06-29 18:34:16 -0300

    Create Exercicio97.py

---
+++ ArquivosPy/Exercicio97.py
@@ -0,0 +1,13 @@
+def escreva(msg):
+    tamanho = len(msg)
+    print('-'*(tamanho+4))
+    print(f'  {msg}  ')
+    print('-'*(tamanho+4))
+
+
+mensagem2 = 'Fuck Bolha TT Libera as cameras dog'
+mensagem3 = 'Seltom ou Selton?'
+mensagem1 = 'Olá Mundo!'
+escreva(mensagem1)
+escreva(mensagem2)
+escreva(mensagem3)
