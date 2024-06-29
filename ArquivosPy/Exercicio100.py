8183d8000dbeccd11fd0e2dcd19905108c83eaf5
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-06-29 18:34:12 -0300

    Create Exercicio100.py

---
+++ ArquivosPy/Exercicio100.py
@@ -0,0 +1,15 @@
+from random import randint
+numeros = []
+def sorteia():
+    for cont in range(0, 5):
+        numeros.append(randint(1,9))
+    print(numeros)
+def SomaPar(num):
+    somavalorespares = 0
+    for valorunico in num:
+        if valorunico % 2 == 0:
+            somavalorespares += valorunico
+    print(somavalorespares)
+
+sorteia()            
+SomaPar(numeros)
