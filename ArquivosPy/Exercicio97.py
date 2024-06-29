e327ae6caa15b8e4b3e170fa86d6a71852dd008c
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-06-29 18:49:05 -0300

    Update Exercicio97.py

--- ArquivosPy/Exercicio97.py
+++ ArquivosPy/Exercicio97.py
@@ -6,8 +6,10 @@ def escreva(msg):
 
 
 mensagem2 = 'Fuck Bolha TT Libera as cameras dog'
-mensagem3 = 'Seltom ou Selton?'
+mensagem3 = 'Seltom ouSelton?'
 mensagem1 = 'Olá Mundo!'
+mensagem4 = 'Sonic me diga. é Hunter Hunter, hunter xis hunter, hunter equis hunter, hunter versus hunter ou hunter vezes hunter?'
 escreva(mensagem1)
 escreva(mensagem2)
 escreva(mensagem3)
+escreva(mensagem4)
