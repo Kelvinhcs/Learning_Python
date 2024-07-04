d95c709c29ed78d1f53e7f69445fd83b1ff0dc17
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-07-04 15:08:48 -0300

    Create Exercicio102.py

---
+++ ArquivosPy/Exercicio102.py
@@ -0,0 +1,14 @@
+from math import factorial
+def fatorial(num, show=False):
+    if show == True:
+        print('Amigao, é só usar a biblioteca tlgdKKKKKKKKKK')
+    return factorial(num)
+
+
+
+
+
+
+
+numero = int(input('Digite um número: '))
+print(fatorial(numero))
