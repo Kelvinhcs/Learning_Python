a0b29271f2427ded5c1234e507960f54e37590f3
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-07-04 15:08:50 -0300

    Create Exercicio104.py

---
+++ ArquivosPy/Exercicio104.py
@@ -0,0 +1,11 @@
+def leiaint(msg):
+    while True:
+        n = input(msg)
+        if n.isdigit() or n[0] == '-' and n[1:].isdigit():
+            return int(n)
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        
+    
+numero = leiaint('Digite um número inteiro: ')
+print(f'Você digitou o número {numero}')
