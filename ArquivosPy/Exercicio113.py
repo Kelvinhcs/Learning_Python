49043893bb27b3b7c3f98c548bb3321aede9f89f
Kelvinhcs <kelvinhcosta@hotmail.com>
2024-07-16 00:16:59 -0300

    Create Exercicio113.py

---
+++ ArquivosPy/Exercicio113.py
@@ -0,0 +1,33 @@
+def LeiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except KeyboardInterrupt:
+            print(f'\033[0;31mO Usuário decidiu não informar este dado, considerando 0...\033[m')
+            return 0
+        except:
+            print('\033[0;31mERRO! Tente digitar o número novamente...\033[m')
+        else:
+            return n
+
+def LeiaFloat(msg):
+    while True:
+        try:
+            n = input(msg)
+            if n.find('.') > -1:
+                n = float(n)
+            else:
+                print('\033[0;31mERRO! Tente digitar o número novamente...\033[m')
+                continue
+        except KeyboardInterrupt:
+            print(f'\033[0;31mO Usuário decidiu não informar este dado, considerando 0...\033[m')
+            return 0
+        except:
+            print('\033[0;31mERRO! Tente digitar o número novamente...\033[m')
+        else:
+            return n
+        
+        
+Inteiro = LeiaInt('Digite um número inteiro: ')
+Flutuante = LeiaFloat('Digite um número com casa decimal: ')
+print(f'O número inteiro digitado foi {Inteiro} e o decimal foi {Flutuante} .')
